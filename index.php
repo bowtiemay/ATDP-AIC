f209d67c58ddc97813a8b804d73eb027f6814efe
Maya Cobb <62029215+mayacobb@users.noreply.github.com>
2021-07-22 18:07:08 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,55 @@
+
+<!DOCTYPE html>
+
+ <html>
+ <head>
+   <title>Maya's AIC Index</title>
+  <body>
+    <h1>My AIC Assignments</h1>
+    <p>
+      <ul>
+        <li><a href = "https://atdpsites.berkeley.edu/mcobb/hw1/index.html">H1: Your First Assignment</a></li>
+        <br>
+        <li><a href = "https://atdpsites.berkeley.edu/mcobb/X1/x1.php">X1: My Favorite Things</a></li>
+        <br>
+        <li><a href = "https://atdpsites.berkeley.edu/mcobb/hw2/main.php">H2: Spoiler Alert</a></li>
+        <br>
+        <li><a href = "https://atdpsites.berkeley.edu/mcobb/x2/gettingtoknowyou.php">X2: Getting to Know You</a></li>
+        <br>
+        <li><a href = "https://atdpsites.berkeley.edu/mcobb/v01/introquestions.php">P1: Initialization (V1)</a></li>
+        <br>
+        <li><a href = "https://atdpsites.berkeley.edu/mcobb/X3/x3.html?name=cat&correction=234">X3: Department of Corrections</a></li>
+        <br>
+        <li><a href = "https://atdpsites.berkeley.edu/mcobb/v02/introquestions.php">P1: Augment 1 (V2)</a></li>
+        <br>
+        <li><a href = "https://atdpsites.berkeley.edu/mcobb/x4/x4.php">X4: Lights Out</a></li>
+        <br>
+        <li><a href = "https://atdpsites.berkeley.edu/mcobb/v03/introquestions.php">P1: Augment 2</a></li>
+        <br>
+        <li><a href = "https://atdpsites.berkeley.edu/fhabib/Assignments/X5_Magic_Mixerator/magicmixerator.html">X5: Magic Mixerator</a></li>
+        <br>
+        <li><a href = "https://atdpsites.berkeley.edu/mcobb/x6/x6.html">X6: Gotta Catch..A Very Specific Subset!</a></li>
+        <br>
+        <li><a href = "https://atdpsites.berkeley.edu/mcobb/v04b/testquestions.php">P1: Augment 3 (V3)</a></li>
+        <br>
+        <li><a href = "https://atdpsites.berkeley.edu/mcobb/X8/x8.html">X8: To JOIN Them is My Cause</a></li>
+        <br>
+        <li><a href = "https://atdpsites.berkeley.edu/mcobb/v-final/testquestions.php">P1: Final</a></li>
+        <br>
+        <li><a href = "https://atdpsites.berkeley.edu/mcobb/P2-outline/p2.html">P2: Outline</a></li>
+        <br>
+        <li><a href = "https://atdpsites.berkeley.edu/mcobb/X9/comic.php">X9: The Chalkboard Manifesto (Parts 1 and 2)</a></li>
+        <br>
+        <li><a href = "https://atdpsites.berkeley.edu/mcobb/X9/archive.php">X9: The Chalkboard Manifesto (Part 3)</a></li>
+        <br>
+        <li><a href = "https://atdpsites.berkeley.edu/mcobb/X10/game.php">X10: Gotta Coach Them All</a></li>
+        <br>
+        <li><a href = "https://atdpsites.berkeley.edu/mcobb/X11/signin.php">X11: Gone With The Windbreaker</a></li>
+        <br>
+        <li><a href = "https://atdpsites.berkeley.edu/p2/P2-A/signin.php">P2: Alpha Version</a></li>
+        <br>
+        <li><a href = "https://docs.google.com/document/d/131VDkXEPG_a70ZqFebGzb9AW7SpaxzBmp_xEP5Hsxhc/edit">Discussion 1: Big Data and YOU</a></li>
+      </ul>
+    </p>
+  </body>
+ </html>
